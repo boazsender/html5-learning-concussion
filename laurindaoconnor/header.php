a0520e8946f69b9c031f07b9cc9467d3610d0422
janmichaelmaluto <janmichaelmaluto@zeray.mca.massart.edu>
2011-03-09 19:33:23 -0500

    just added some php

---
+++ laurindaoconnor/header.php
@@ -0,0 +1,27 @@
+<header>
+  <div class="span-18">
+    <ul>
+      <li class="span-4">
+        <a href="">Projects</a>
+        <ul>
+          <li><a href="">Residential</a></li>
+          <li><a href="">Commercial | Institutional</a></li>
+          <li><a href="">On the Boards</a></li>
+        </ul>
+      </li>
+      <li class="span-4">
+        <a href="">Office</a>
+        <ul>
+          <li><a href="">About Us</a></li>
+          <li><a href="">Resumes</a></li>
+          <li><a href="">Media</a></li>
+        </ul>
+      </li>
+      <li class="span-4"><a href="">Contact</a></li>
+    </ul>
+  </div>
+  <div class="span-6 last">
+    <h3 class="span-6 last">Koffka | Phakos Design</h3>
+  </div>
+</header> 
+
